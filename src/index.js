49ef67f661eba7fa7c48e856139c35ca6c77c2d7
Rodrigo Bussolo <rbussolo91@gmail.com>
2021-03-14 09:54:15 -0400

    Adicionado os middlewares.

--- src/index.js
+++ src/index.js
@@ -10,19 +10,92 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  // Carrega o username que vem pelo header
+  const { username } = request.headers;
+
+  // Verifica se existe este usuário
+  const user = users.find((user) => user.username === username);
+
+  // Caso não tenha sido localizado, retorna um erro
+  if(!user){
+    return response.status(404).json({ error: "User not found!" });
+  }
+
+  // Adiciona este usuário na requisição
+  request.user = user;
+
+  // Passa para a proxima etapa
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  // Carrega usuário que esta nesta requisição
+  const { user } = request;
+
+  // Verifica se este usuário é plano GRATIS e já tem 10 tarefas
+  if(!user.pro && user.todos.length == 10){
+    return response.status(403).json({ error: "User already has 10 todos, necessary update account to PRO to create a new todo." });
+  }
+
+  // Avança para proxima etapa
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  // Carrega o username que vem pelo header
+  const { username } = request.headers;
+
+  // Carrega o ID do todo que esta sendo consultado
+  const { id } = request.params;
+  
+  // Valida se o ID fornecido é no formado uuid
+  const validId = validate(id);
+
+  if(!validId){
+    return response.status(400).json({ error: "Not valid todo ID!" });
+  }
+
+  // Verifica se existe este usuário
+  const user = users.find((user) => user.username === username);
+
+  // Caso não tenha sido localizado, retorna um erro
+  if(!user){
+    return response.status(404).json({ error: "User not found!" });
+  }
+
+  // Verifica se localiza este todo na lista de tarefas deste usuário
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  // Caso não tenha sido localizado a tarefa retorna um erro
+  if(!todo){
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  // Adiciona na requisição o usuário e a tarefa
+  request.user = user;
+  request.todo = todo;
+
+  // Avança para proxima etapa
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  // Carrega o username que vem pelo header
+  const { id } = request.params;
+
+  // Verifica se existe este usuário
+  const user = users.find((user) => user.id === id);
+
+  // Caso não tenha sido localizado, retorna um erro
+  if(!user){
+    return response.status(404).json({ error: "User not found!" });
+  }
+
+  // Adiciona este usuário na requisição
+  request.user = user;
+
+  // Passa para a proxima etapa
+  return next();
 }
 
 app.post('/users', (request, response) => {
